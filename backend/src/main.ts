98159b3e2f625dae6823a222c1d4387f3fec5470
cupcake <mohammd.alkhamisi@gmail.com>
2024-12-05 15:22:06 +0000

    init prisma and swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // Build Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Apartment Browser REST API')
+    .setDescription('Apartment Browser REST API')
+    .setVersion('1.0.0')
+    .build();
+
+  SwaggerModule.setup(
+    'docs',
+    app,
+    SwaggerModule.createDocument(app, config, {
+      operationIdFactory: (_, methodKey: string) => methodKey,
+    }),
+    {
+      customSiteTitle: 'Apartment Browser REST API',
+    },
+  );
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
